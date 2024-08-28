ac09693549343c75f8b78f446ee9b5eb2971613d
NoeCecillon <noe.cecillon@gmail.com>
2024-08-28 15:37:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from SG2V import *
 from WSGCN import *
+from SGCN import *
+from SiNE import *
 from evaluation import *
 
 
 run_all_experiments_sg2v()
 run_all_experiments_wsgcn()
+run_sgcn()
+run_sine()
+
 
 #evaluation
 embeddings = load_embeddings(path_emb)
